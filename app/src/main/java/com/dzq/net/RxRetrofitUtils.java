567417b7d767fe313286c5a2ad5c30c29435485e
daizeqian <daizeqian@biulove.com>
2018-12-27 15:18:01 +0800

    retrofit client update

---
+++ app/src/main/java/com/dzq/net/RxRetrofitUtils.java
@@ -0,0 +1,10 @@
+package com.dzq.net;
+
+/**
+ * Created by admin on 2018/12/27.
+ * 配合Rxjava整合的Retrofit框架
+ */
+
+public class RxRetrofitUtils {
+
+}
--- app/src/main/java/com/dzq/net/client/RetrofitClient.java
+++ app/src/main/java/com/dzq/net/client/RetrofitClient.java
@@ -1,5 +1,7 @@
 package com.dzq.net.client;
 
+import com.dzq.net.config.OkHttpConfig;
+import com.dzq.net.config.RetrofitConfig;
 import com.dzq.net.https.SSLConfig;
 import com.dzq.net.interceptor.RtLoggerInterceptor;
 
@@ -7,7 +9,12 @@ import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.CallAdapter;
+import retrofit2.Converter;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 /**
  * Created by admin on 2018/12/26.
@@ -39,8 +46,47 @@ public class RetrofitClient {
         initOkhttp();
 
         mRBuilder = new Retrofit.Builder();
+        CallAdapter.Factory[] callAdapterFactorys = RetrofitConfig.getInstance().getCallAdapterFactory();
+        if (callAdapterFactorys != null && callAdapterFactorys.length > 0) {
+            for (CallAdapter.Factory factory : callAdapterFactorys) {
+                mRBuilder.addCallAdapterFactory(factory);
+            }
+        } else {
+            mRBuilder.addCallAdapterFactory(RxJava2CallAdapterFactory.create());
+        }
+
+        Converter.Factory[] callAdapterConver = RetrofitConfig.getInstance().getConverterFactory();
+        if (callAdapterConver != null && callAdapterConver.length > 0) {
+            for (Converter.Factory conver : callAdapterConver) {
+                mRBuilder.addConverterFactory(conver);
+            }
+        } else {
+            mRBuilder.addConverterFactory(ScalarsConverterFactory.create()).
+                      addConverterFactory(GsonConverterFactory.create());
+        }
     }
 
+    /**
+     * 获取Retrofit Builder 对象
+     *
+     * @return
+     */
+    public Retrofit.Builder getRetrofitBuilder() {
+        return mRBuilder;
+    }
+
+    public Retrofit getRetrofit() {
+        if (OkHttpConfig.getOkHttpClient() == null) {
+            return mRBuilder.client(okHttpClient).build();
+        } else {
+            return mRBuilder.client(OkHttpConfig.getOkHttpClient()).build();
+        }
+    }
+
+
+    /**
+     * 初始化OKhttp
+     */
     private void initOkhttp() {
         OkHttpClient.Builder builder = new OkHttpClient.Builder();
         //配置默认信息
